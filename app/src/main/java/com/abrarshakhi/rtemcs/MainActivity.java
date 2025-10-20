45009bc08064adfac8df2e4d617e2a9df1c0d153
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-10-20 20:02:25 +0600

    chnage colors, remove toolbar etc

--- app/src/main/java/com/abrarshakhi/rtemcs/MainActivity.java
+++ app/src/main/java/com/abrarshakhi/rtemcs/MainActivity.java
@@ -1,19 +1,22 @@
 package com.abrarshakhi.rtemcs;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.abrarshakhi.rtemcs.databinding.ActivityMainBinding;
-import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ArrayList<String> deviceList;
+    private ArrayAdapter<String> deviceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,48 +25,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        deviceList = new ArrayList<>();
+        deviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceList);
 
-        binding.fab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Snackbar.make(
-                                        view,
-                                        "Unimplemented: It will add a device to to monitor",
-                                        Snackbar.LENGTH_LONG
-                                )
-                                .setAnchorView(R.id.fab)
-                                .setAction("Action", null)
-                                .show();
-                    }
-                });
-    }
+        binding.lvListOfDeviceMain.setAdapter(deviceAdapter);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        binding.btnAddDeviceMain.setOnClickListener(v -> {
+            String newDevice = "Device " + (deviceList.size() + 1);
+            deviceList.add(newDevice);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+            deviceAdapter.notifyDataSetChanged();
 
-        if (id == R.id.action_settings) {
-            Toast.makeText(
-                    this,
-                    "Unimplemented: Settings",
-                    Toast.LENGTH_LONG
-            ).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        return super.onSupportNavigateUp();
+            Toast.makeText(MainActivity.this, newDevice + " added!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
